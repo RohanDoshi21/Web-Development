68d61d6415b2f26ff8332a0f9260c60dbf7d1483
Rohan Doshi <rohan.doshi02@gmail.com>
2021-07-21 23:26:44 +0530

    Learning js

---
+++ Javascript Sig/test.js
@@ -0,0 +1,45 @@
+// // console.log("Hello World");
+//!variables
+// var a = 10;
+// a = a + 10;
+// console.log(a);
+
+
+
+//!arrays
+// // const classes = ['fe01', 'fe2', 'fe3'];
+// // classes.push('fe5');
+// // console.log(classes);
+
+
+//! locked arrays
+// // lock array
+// const classes = Object.freeze['fe', 'se','te'];
+// console.log(classes)
+// // classes.push('be');    now we have freezed the array
+// console.log(classes);
+
+
+
+//! functions
+// // normal functions
+// function add_no(a,b){
+//     return a+b;
+// }
+
+// arrow functions
+
+// const arrow_add = (a,b) =>{
+//     return a+b;
+// }
+
+// console.log(add_no(3,5));
+// console.log(arrow_add(3,5));
+
+
+
+//!json objects
+// const car = {name:"I20", tpye:"Hackback"};
+// console.log(car);
+// console.log(car.name);
+// console.log(car['name']);
